5d645f11d3e082ed82d2253c054e53b1b2871f30
Luiz Felipe <lfelipecx@gmail.com>
2025-03-07 20:37:11 -0300

    feat: add new markdown to jsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/app/_components/ui/button";
 import { getPost } from "@/app/_services/notion";
 import Link from "next/link";
-import ReactMarkdown from "react-markdown";
+import Markdown from "markdown-to-jsx";
 
 
 type Params = Promise<{ slug: string }>
@@ -17,7 +17,7 @@ const BlogPost = async (  { params }: { params: Params } ) => {
         <h1 className="text-3xl text-center md:text-4xl py-6 text-gray-700 font-semibold">
           {post.title}
         </h1>
-        <ReactMarkdown className="prose dark:prose-invert">{post.content}</ReactMarkdown>
+        <Markdown className="prose dark:prose-invert">{post.content}</Markdown>
         <div className="flex justify-center pt-8">
           <Button asChild>
             <Link href="/blog" className="font-semibold">Voltar</Link>
